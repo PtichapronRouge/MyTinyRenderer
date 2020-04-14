7301a9a0cf00209ca6ce72e0810139fe454a6d39
PtichapronRouge <benjamin.mouscadet@supelec.fr>
2020-04-14 22:14:26 +0200

    changed main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,27 @@
 #include "tgaimage.h"
 #include "line.hpp"
+#include "triangle.hpp"
+#include "objmodel.hpp"
 
 const TGAColor white = TGAColor(255,255,255,0);
 const TGAColor red   = TGAColor(255,0,0,0);
 const TGAColor blue  = TGAColor(0,0,255,0);
-
+const TGAColor green = TGAColor(0,255,0,0);
 
 int main(int argc, char* argv[])
 {
-    int x0 = 0,
-        y0 = 0,
-        x1 = 100,
-        y1 = 100;
-    if(argc == 5) {
-        x0 = std::stoi(argv[1]),
-        y0 = std::stoi(argv[2]),
-        x1 = std::stoi(argv[3]),
-        y1 = std::stoi(argv[4]);
-    }
+    TGAImage image(200,200, TGAImage::RGB);
 
-    TGAImage image(100,100, TGAImage::RGB);
-    plot_line(x0,y0, x1,y1, image,red);
-    plot_line(x1,y1, x0,y0, image,blue);
+    Vec2i t0[3] = {Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80)};
+    Vec2i t1[3] = {Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180)};
+    Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)};
+    //empty_triangle(t0[0], t0[1], t0[2], image, white);
+    //empty_triangle(t1[0], t1[1], t1[2], image, white);
+    //empty_triangle(t2[0], t2[1], t2[2], image, white);
+    fill_triangle(t0[0], t0[1], t0[2], image, blue);
+    fill_triangle(t1[0], t1[1], t1[2], image, red);
+    fill_triangle(t2[0], t2[1], t2[2], image, green);
+
     image.flip_vertically();
     image.write_tga_file("output.tga");
 
